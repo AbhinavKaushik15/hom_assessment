137630261be7bd065901d47a969a3ceccb9c128a
Abhinav Kaushik <abhinavsharmaas@gmail.com>
2025-03-23 13:08:59 +0530

    nav text

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,22 +4,22 @@ import { IoMenuOutline } from "react-icons/io5";
 
 function Navbar() {
   return (
-    <div className="fixed top-0 left-0 z-[10] w-full h-14 bg-[#1d1b1b7e] flex items-center justify-between text-zinc-400 px-6">
+    <div className="fixed top-0 left-0 z-[10] w-full h-14 bg-[#1d1b1b7e] flex items-center justify-between text-zinc-400 px-4 md:px-6">
       {/* logo */}
-      <h1 className="logo text-zinc-600 text-[2.5vh] md:text-[1.7vw] font-[500] leading-0">
+      <h1 className="logo text-zinc-600 text-[2.4vh] tracking-tighter md:text-[1.7vw] font-[500] leading-0">
         House of Marktech
       </h1>
 
       {/* NavLinks */}
       <div className="flex gap-[5vw] items-center">
         <NavLink
-          className="flex text-[2.2vh] md:text-[2.8vh] hover:text-black"
+          className="flex text-[2.1vh] md:text-[2.8vh] hover:text-black"
           to="/"
         >
           Home
         </NavLink>
         <NavLink
-          className="flex text-[2.2vh] md:text-[2.8vh] hover:text-black"
+          className="flex text-[2.1vh] md:text-[2.8vh] hover:text-black"
           to="/users"
         >
           Users
