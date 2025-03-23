021b82a71e41185f74b8e21f27e8177376dbd6c1
Abhinav Kaushik <abhinavsharmaas@gmail.com>
2025-03-23 13:04:22 +0530

    again nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,20 +6,20 @@ function Navbar() {
   return (
     <div className="fixed top-0 left-0 z-[10] w-full h-14 bg-[#1d1b1b7e] flex items-center justify-between text-zinc-400 px-6">
       {/* logo */}
-      <h1 className="logo text-zinc-600 md:text-[1.7vw] font-[500] leading-0">
+      <h1 className="logo text-zinc-600 text-[2.5vh] md:text-[1.7vw] font-[500] leading-0">
         House of Marktech
       </h1>
 
       {/* NavLinks */}
       <div className="flex gap-[5vw] items-center">
         <NavLink
-          className="flex text-[2.4vh] md:text-[2.8vh] hover:text-black"
+          className="flex text-[2.2vh] md:text-[2.8vh] hover:text-black"
           to="/"
         >
           Home
         </NavLink>
         <NavLink
-          className="flex text-[2.4vh] md:text-[2.8vh] hover:text-black"
+          className="flex text-[2.2vh] md:text-[2.8vh] hover:text-black"
           to="/users"
         >
           Users
